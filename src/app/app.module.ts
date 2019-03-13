a9ad171bd3c66dc72d7fc139b81a6341509725b4
vijayaprag <31339437+vijayaprag@users.noreply.github.com>
2019-03-13 15:33:09 +0530

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,10 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpModule } from '@angular/http';
+import { AppComponent } from './app.component';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
@@ -10,7 +14,9 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+	  BrowserAnimationsModule,
+      HttpModule
   ],
   providers: [],
   bootstrap: [AppComponent]
