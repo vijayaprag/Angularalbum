9657928493303b9c504c199ea0acd81899b6fb32
vijayaprag <31339437+vijayaprag@users.noreply.github.com>
2019-03-13 15:32:40 +0530

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
 import { Component } from '@angular/core';
 
+import { Http } from '@angular/http';
+
+import { map, filter, scan } from 'rxjs/operators';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'Angularalbum';
+ title = 'Angularalbum';
+  constructor(private http: Http) { }
+
+httpdata;
+   ngOnInit() {
+      this.http.get("http://jsonplaceholder.typicode.com/albums")
+      .pipe(map((response) => response.json()))
+      .subscribe((data) => this.displaydata(data));     
+   }
+   displaydata(data) {this.httpdata = data;}
 }
